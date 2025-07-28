d8d6e79c08e5c2e99e45095623e3188ad11b6332
Darcy Liu <darcy.ldx@gmail.com>
2025-07-28 10:11:06 -0400

    Deploy AWS Cognito authentication with real resource IDs

--- amplify.ts
+++ amplify.ts
@@ -6,9 +6,9 @@ import type { Schema } from './amplify/data/resource';
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: 'us-east-2_aurasight',
-      userPoolClientId: 'aurasight-client',
-      identityPoolId: 'us-east-2:aurasight-identity-pool',
+      userPoolId: 'us-east-2_dELxg3zLc',
+      userPoolClientId: '4mu9955gchebq3etpvu504b5q6',
+      identityPoolId: 'us-east-2:add80c06-1d98-4306-9647-e2fa7c97c12c',
       signUpVerificationMethod: 'code',
       loginWith: {
         email: true,
@@ -17,7 +17,7 @@ Amplify.configure({
   },
   API: {
     GraphQL: {
-      endpoint: 'https://aurasight-graphql-endpoint.amazonaws.com/graphql',
+      endpoint: 'https://e7tr74u2gffl7fkntmuzat6f6a.appsync-api.us-east-2.amazonaws.com/graphql',
       region: 'us-east-2',
       defaultAuthMode: 'userPool',
     },
